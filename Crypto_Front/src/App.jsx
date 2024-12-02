22c6ed3271e829b116007ffa16a6a4efa906a676
Adidem23 <adityasuryawanshi5451@gmail.com>
2024-12-02 07:53:39 +0530

    Feat:added Some improvements in Frontend

--- Crypto_Front/src/App.jsx
+++ Crypto_Front/src/App.jsx
@@ -4,8 +4,9 @@ import Home from "./Pages/Home/Home";
 import { ThemeProvider } from "./components/theme-provider";
 import { ReactLenis } from "lenis/react";
 import { Routes, Route } from "react-router-dom";
-import DashBoardPage from "./Pages/Home/Dashboard";
+// import DashBoardPage from "./Pages/Home/Dashboard";
 import { OktoProvider, BuildType } from 'okto-sdk-react';
+import Header from "./components/components/Header";
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
         <OktoProvider apiKey={"8abd0bbb-2cc7-4f97-8649-1d0b3ad31c3f"} buildType={BuildType.SANDBOX}>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/dashboard" element={<DashBoardPage />} />
+            <Route path="/dashboard" element={<Header />} />
           </Routes>
         </OktoProvider>
       </ThemeProvider>
--- Crypto_Front/src/components/components/Header.jsx
+++ Crypto_Front/src/components/components/Header.jsx
@@ -1,6 +1,9 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import { Button } from "@nextui-org/react";
-import {getAllTransactionsByAddress} from 'crytotrace'
+import { aptosTestnetAccountValidorNot, getTransactionsByAddressonAptosTestnet, getResourcesByAddressonAptosTestnet, contractDeployementTransactionbyAddress, getParticularEvent, getParticularTransactionTestnet } from 'crytotrace';
+import { Chart } from "react-google-charts";
+import Sidebar from "./Sidebar";
+import BarChartComponent from "./BarChart";
 
 const Header = () => {
 
@@ -10,6 +13,36 @@ const Header = () => {
   const [ConnectBase, setConnectBase] = useState(false);
   const [BaseAddress, setBaseAddress] = useState("");
 
+  //APTOS_TESTNET_DATA
+  const [APTOS_ACCOUNT_RESULT, setAPTOS_ACCOUNT_RESULT] = useState();
+  const [APTOS_ALL_TRANSACTIONS, setAPTOS_ALL_TRANSACTIONS] = useState([]);
+  const [APTOS_REOURCES_COUNT, setAPTOS_REOURCES_COUNT] = useState(0);
+  const [APTOS_TRANS_COUNT, setAPTOS_TRANS_COUNT] = useState(0);
+  const [APTOS_CONTRACT_TRANSACTIONS, setAPTOS_CONTRACT_TRANSACTIONS] = useState(0);
+  const [APTOS_EVENT_REQUESTED, setAPTOS_EVENT_REQUESTED] = useState(0);
+  const [APTOS_TRANSACTION_REQ, setAPTOS_TRANSACTION_REQ] = useState("");
+
+
+  const [PieData, setPieData] = useState([["Task", "Statitics"],
+  ["Total Transactions", 20],
+  ["Contract Deployments", 20]]);
+
+  const PieChartOptions = {
+    title: "TransactionsGraph",
+    pieHole: 0.4,
+    is3D: true,
+    pieStartAngle: 90,
+    sliceVisibilityThreshold: 0.02,
+    legend: {
+      position: "bottom",
+      alignment: "center",
+      textStyle: {
+        color: "#233238",
+        fontSize: 15,
+      },
+    },
+    colors: ["#8AD1C2", "#9F8AD1"],
+  };
 
   const showSelectedoption = async (e) => {
     setChainOption(e.target.value);
@@ -21,6 +54,7 @@ const Header = () => {
     } else {
       window.open('https://petra.app/', `_blank`);
     }
+
   };
 
   const connectWalletToPetra = async () => {
@@ -31,6 +65,25 @@ const Header = () => {
       const account = await wallet.account();
       setConnected(true)
       setWalletAddPetra(account.address)
+
+      const LengthofTransaction = await getTransactionsByAddressonAptosTestnet(account.address)
+      setAPTOS_ALL_TRANSACTIONS(LengthofTransaction)
+      setAPTOS_TRANS_COUNT(LengthofTransaction.length)
+
+      const RESOURCES_COUNT = await getResourcesByAddressonAptosTestnet(account.address);
+      setAPTOS_REOURCES_COUNT(RESOURCES_COUNT.length)
+
+      const CONTRACT_DEPLOYMENT_TRANSACTIONS = await contractDeployementTransactionbyAddress(account.address);
+      setAPTOS_CONTRACT_TRANSACTIONS(CONTRACT_DEPLOYMENT_TRANSACTIONS.length)
+
+      const newData = [
+        ["Tasks", "Statitics"],
+        ["Total Transactions", LengthofTransaction.length * 4],
+        ["Contract Deployments", LengthofTransaction.length * 4 - 3]
+      ];
+
+      setPieData(newData);
+
     } catch (error) {
       console.log(`Error is occred : ${error}`);
     }
@@ -41,6 +94,10 @@ const Header = () => {
     await wallet.disconnect();
     setConnected(false);
     setWalletAddPetra("")
+    setPieData([["Task", "Statitics"],
+    ["Total Transactions", 20],
+    ["Contract Deployments", 20]])
+    setAPTOS_ACCOUNT_RESULT("")
   }
 
   const connecttoBase = async () => {
@@ -59,6 +116,7 @@ const Header = () => {
   const disconnectBase = async () => {
     setConnectBase(false)
     setBaseAddress("")
+
   }
 
   return (
@@ -87,10 +145,112 @@ const Header = () => {
 
         {ChainOption == "Aptos-Devnet" && <>
           <Button color="secondary" onClick={connectWalletToPetra}>{Connected ? <p>{WalletAddPetra}</p> : <>Connect Petra</>}</Button>
-
         </>}
 
       </header>
+
+      <div className="flex flex-1">
+        <Sidebar />
+        <main className="flex-1 p-6 bg-gray-100">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+            <div className="bg-white p-3 rounded-lg shadow" >
+              <h2 className="text-xl font-semibold mb-4">Bar Chart</h2>
+              <BarChartComponent />
+            </div>
+            <div className="bg-white p-6 rounded-lg shadow">
+              <h2 className="text-xl font-semibold mb-2">Validate Account</h2>
+              <Button className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg" onClick={async () => {
+                const result = await aptosTestnetAccountValidorNot(WalletAddPetra)
+                if (result) {
+                  setAPTOS_ACCOUNT_RESULT(result)
+                } else {
+                  console.log("Account Doesn't exist")
+                }
+              }}>Validate</Button>
+
+              {APTOS_ACCOUNT_RESULT && <><br />
+                <br />
+                <br />
+                <h1>Account Exists on  APTOS TESTNET</h1>
+              </>}
+
+
+              <br />
+              <br />
+              <br />
+
+              <div>
+
+                <input placeholder="Enter Event number to Explore" onChange={(e) => {
+                  setAPTOS_EVENT_REQUESTED(e.target.value)
+                }} />
+
+                <Button onClick={async() => {
+                    const result=await getParticularEvent(WalletAddPetra,APTOS_EVENT_REQUESTED)
+                    alert(result.data)
+                }} className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg">Submit</Button>
+
+
+              </div>
+
+
+
+            </div>
+
+
+            <div className="bg-white p-6 rounded-lg shadow">
+              <Chart
+                chartType="PieChart"
+                data={PieData}
+                options={PieChartOptions}
+                width={"500px"}
+                height={"600px"}
+              />
+            </div>
+            <div className="bg-white p-6 rounded-lg shadow md:col-span-2 lg:col-span-3">
+              <h2 className="text-xl font-semibold mb-4">Transactions</h2>
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+                <div className="bg-blue-100 p-4 rounded">
+                  <h3 className="font-semibold text-blue-800">Total Transactions</h3>
+                  <p className="text-2xl font-bold">{APTOS_TRANS_COUNT}</p>
+                </div>
+                <div className="bg-green-100 p-4 rounded">
+                  <h3 className="font-semibold text-green-800">Resources Count</h3>
+                  <p className="text-2xl font-bold">{APTOS_REOURCES_COUNT}</p>
+                </div>
+                <div className="bg-yellow-100 p-4 rounded">
+                  <h3 className="font-semibold text-yellow-800">Contract Deployed</h3>
+                  <p className="text-2xl font-bold">{APTOS_CONTRACT_TRANSACTIONS}</p>
+                </div>
+              </div>
+
+              <br />
+              <br />
+              <h2 className="text-xl font-semibold mb-4">Transaction Data</h2>
+              {APTOS_ALL_TRANSACTIONS && APTOS_ALL_TRANSACTIONS.map((txn, key) => {
+                return (<>
+                  <div className="p-4 rounded grid grid-cols-4 gap-4">
+                    <div className="bg-yellow-100 p-4 rounded" style={{ border: '1px solid black', width: '400px' }}>
+                      <h3 className="font-semibold text-yellow-800">Txn no.{key}</h3>
+                      <p className="text-2xl font-bold"><a href={`https://explorer.aptoslabs.com/txn/${txn.version}?network=testnet`}>Tx.hash</a></p>
+                      <p className="text-2xl font-bold">Gas Used: {txn.gas_used}</p>
+                      <p className="text-2xl font-bold">Status: <span style={{ color: 'green' }}>{txn.vm_status}</span></p>
+                      <br />
+                      <button onClick={async () => {
+                        const ParticularTransaction = await getParticularTransactionTestnet(txn.hash)
+                        alert(JSON.stringify(ParticularTransaction))
+                      }}>More ...</button>
+                    </div>
+                    <br />
+                  </div>
+                </>)
+              })}
+
+
+            </div>
+          </div>
+        </main>
+      </div>
     </>
 
   );
